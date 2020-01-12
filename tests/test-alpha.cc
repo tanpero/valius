1c88e08387958d2f67233d137aaca5cb8c13ec14
tanpero <3185750139@qq.com>
2020-01-12 10:22:31 +0800

    minor update

--- tests/test-alpha.cc
+++ tests/test-alpha.cc
@@ -5,15 +5,15 @@ int main(int argc, char* argv[])
 {
 	using namespace valius::alpha;
 	if (argc == 2)
-		std::cout << '"' << argv[1] << (isAlpha(argv[1]) ? "\" is " : "is not ") << "alpha" << "\n";
+		std::cout << '"' << argv[1] << (isAlpha(argv[1]) ? "\" is " : "\" is not ") << "alpha" << "\n";
 	else if (argc == 3)
 		try
 		{
-			std::cout << '"' << argv[1] << (isAlpha(argv[1], argv[2]) ? "\" is " : "is not ") << "alpha" << "\n";
+			std::cout << '"' << argv[1] << (isAlpha(argv[1], argv[2]) ? "\" is " : "\" is not ") << "alpha" << "\n";
 		}
 		catch (const std::exception&)
 		{
-			std::cout << "Unknown language name: " << argv[2] << "\n";
+			std::cout << "\"" << argv[2] << "\" is not a supported language type.\n";
 		}
 	else
 		std::cout << "Usage: " + std::string(argv[0]) + " <text> <language>" << "\n";
